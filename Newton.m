24ba5b85ef20e8ec69d9c767d86816304553cf4e
PasqM <pasqmigl97@gmail.com>
2022-01-23 17:29:58 +0100

    Debug Newton.m

--- Newton.m
+++ Newton.m
@@ -1,24 +1,14 @@
-clear
-clc
-x=89;
-y=-58;
-J=[4*(x - 2)^3 + y^2*(2*x - 4);2*y + 2*y*(x - 2)^2 + 2];
-H=[12*(x - 2)^2 + 2*y^2, 4*(x-2)*y;4*(x-2)*y, 2*(x - 2)^2 + 2];
-n=10;
-mat=zeros(n,2);
-mat(1,2)=y;
-mat(1,1)=x;
-j=0;
-while(norm(J)>0.01)
- for i=2:n
-        J=[4*(mat(i,1) - 2)^3 + mat(i,2)^2*(2*mat(i,1) - 4);2*mat(i,2) + 2*mat(i,2)*(mat(i,1) - 2)^2 + 2];
-        H=[12*(mat(i,1) - 2)^2 + 2*mat(i,2)^2, 4*(mat(i,1)-2)*mat(i,2);4*(mat(i,1)-2)*mat(i,2), 2*(mat(i,1) - 2)^2 + 2];
-        p=H\J;
-        mat(i,:) = mat(i,:) - p';
-        if (norm(mat(i,:)-mat(i-1,:))<0.01*(1+norm(mat(i,:))))
-            break;
-        end
-        
- end
- j=j+1;
+maxit=10000;
+tol=1e-8;
+mat=zeros(maxit+1,2);
+mat(1,2)=1;
+mat(1,1)=1;
+for i=1:maxit
+    J=[4*(mat(i,1) - 2)^3 + mat(i,2)^2*(2*mat(i,1) - 4);2*mat(i,2) + 2*mat(i,2)*(mat(i,1) - 2)^2 + 2];
+    H=[12*(mat(i,1) - 2)^2 + 2*mat(i,2)^2, 4*(mat(i,1)-2)*mat(i,2);4*(mat(i,1)-2)*mat(i,2), 2*(mat(i,1) - 2)^2 + 2];
+    p=H\J;
+    mat(i+1,:) = mat(i,:)-p';
+    if (norm(mat(i+1,:)-mat(i,:))<tol*(1+norm(mat(i+1,:))) && norm(J)<tol)
+        break;
+    end
 end
