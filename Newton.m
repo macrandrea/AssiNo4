4658efe6b793b043da2dbd8cd7f5a71928e7f57a
macrandrea <58860052+macrandrea@users.noreply.github.com>
2022-01-25 17:25:07 +0100

    ES1

--- Newton.m
+++ Newton.m
@@ -12,12 +12,4 @@ for i=1:maxit
     if (norm(mat(i+1,:)-mat(i,:))<tol*(1+norm(mat(i+1,:))) && norm(J)<tol)
         break;
     end
-    j=j+1;
-    %Ef(i)=(mat(i,1)-2)^4+(mat(i,1)-2)^2*mat(i,2)^2+(mat(i,2)+1)^2;
-    hold on
-    %plot([j-1 j], [mat(j-1,:) mat(j,:) ], 'r-');
-    %fprintf('\n Valor= %8.3f  ',mat(i,:))
-    %plot3(Ef(i),mat(i,1),Ef(i),mat(i,2), 'r-')
-    %fplot (0)
-    hold off
 end
