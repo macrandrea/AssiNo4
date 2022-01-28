051309b41001fa9bf66f62f7a29ebfdc4571eac8
macrandrea <58860052+macrandrea@users.noreply.github.com>
2022-01-28 17:33:01 +0100

    Add files via upload

--- NewtonEs3.m
+++ NewtonEs3.m
@@ -1,22 +1,22 @@
 clc,clear
-maxit=100;
+maxit=1000;
 tol=1e-8;
 mat=zeros(maxit+1,2);
 alpha=ones(maxit,1);
 x0=[8,0.2];
 mat(1,:)=x0;
-delta=0.5;
-epsilon=0.5;
+delta=0.2;
+epsilon=0.2;
 for i=1:maxit
-    J=[4*(mat(i,1) - 2)^3 + mat(i,2)^2*(2*mat(i,1) - 4);2*mat(i,2) + 2*mat(i,2)*(mat(i,1) - 2)^2 + 2];
-    H=[12*(mat(i,1) - 2)^2 + 2*mat(i,2)^2, 4*(mat(i,1)-2)*mat(i,2);4*(mat(i,1)-2)*mat(i,2), 2*(mat(i,1) - 2)^2 + 2];
+    H=[2*(mat(i,2)^2 - 1)^2 + 2*(mat(i,2)^3 - 1)^2, 4*mat(i,2)*(mat(i,1)*(mat(i,2)^2 - 1) + 9/4) - 2*mat(i,2) + 6*mat(i,2)^2*(mat(i,1)*(mat(i,2)^3 - 1) + 21/8) + 4*mat(i,1)*mat(i,2)*(mat(i,2)^2 - 1) + 6*mat(i,1)*mat(i,2)^2*(mat(i,2)^3 - 1) + 2; 4*mat(i,2)*(mat(i,1)*(mat(i,2)^2 - 1) + 9/4) - 2*mat(i,2) + 6*mat(i,2)^2*(mat(i,1)*(mat(i,2)^3 - 1) + 21/8) + 4*mat(i,1)*mat(i,2)*(mat(i,2)^2 - 1) + 6*mat(i,1)*mat(i,2)^2*(mat(i,2)^3 - 1) + 2 ,8*mat(i,1)^2*mat(i,2)^2 - 2*mat(i,1) + 18*mat(i,1)^2*mat(i,2)^4 + 4*mat(i,1)*(mat(i,1)*(mat(i,2)^2 - 1) + 9/4) + 12*mat(i,1)*mat(i,2)*(mat(i,1)*(mat(i,2)^3 - 1) + 21/8)];
+    J=[ 2*(mat(i,2)^2 - 1)*(mat(i,1)*(mat(i,2)^2 - 1) + 9/4) + 2*(mat(i,2)^3 - 1)*(mat(i,1)*(mat(i,2)^3 - 1) + 21/8) - (mat(i,2) - 1)^2; 6*mat(i,1)*mat(i,2)^2*(mat(i,1)*(mat(i,2)^3 - 1) + 21/8) - mat(i,1)*(2*mat(i,2) - 2) + 4*mat(i,1)*mat(i,2)*(mat(i,1)*(mat(i,2)^2 - 1) + 9/4)];
     p=H\J;
     mat(i+1,:) = mat(i,:)-alpha(i)*p';
     EfPO=(1.5-mat(i+1,1)*(1-mat(i+1,2))^2)+(2.25-mat(i+1,1)*(1-mat(i+1,2)^2))^2+(2.625-mat(i+1,1)*(1-mat(i+1,2)^3))^2;
     Ef  =(1.5-mat(i,1)*(1-mat(i,2))^2)+(2.25-mat(i,1)*(1-mat(i,2)^2))^2+(2.625-mat(i,1)*(1-mat(i,2)^3))^2;
-    if(EfPO< Ef+delta*alpha(i)*sum(J'*p))
+    if(EfPO > Ef+delta*alpha(i)*sum(p'*J))
         alpha(i+1)=alpha(i)*epsilon;
-        if (alpha(i)<0.1)
+        if (alpha(i)<0.19)
             alpha(i)=alpha(i-1);
         end
     end
@@ -24,3 +24,6 @@ for i=1:maxit
         break;
     end
 end
+
+% f= @(x,y) (1.5-x*(1-y)^2)+(2.25-x*(1-y^2))^2+(2.625-x*(1-y^3))^2;
+% hes=hessian(f);
