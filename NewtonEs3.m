d295675962f97609332bc0916fecdc0ef8652753
macrandrea <58860052+macrandrea@users.noreply.github.com>
2022-01-29 20:30:26 +0100

    Update:TRust_REgion_INcompleto

--- NewtonEs3.m
+++ NewtonEs3.m
@@ -3,17 +3,34 @@ maxit=50;
 tol=1e-8;
 mat=zeros(maxit+1,2);
 alpha=ones(maxit,1);
-x0=[8,0.8];
+x0=[8,0.2];
 mat(1,:)=x0;
 delta=0.25;
 epsilon=0.75;
+eta=0.02;
+mu=0.5;
+em=[]; %da definire
 for i=1:maxit
     H=[2*(1 - mat(i,2)^2)^2 + 2*(1 - mat(i,2)^3)^2, 2* (1 - mat(i,2)) - 4 *mat(i,1)* mat(i,2)* (1 - mat(i,2)^2) - 6* mat(i,1)* mat(i,2)^2* (1 - mat(i,2)^3) + 4 *mat(i,2) *(2.25 - mat(i,1)* (1 - mat(i,2)^2)) + 6 *mat(i,2)^2* (2.625 - mat(i,1) *(1 - mat(i,2)^3)); 2 *(1 - mat(i,2)) - 4* mat(i,1)* mat(i,2) *(1 - mat(i,2)^2) - 6 *mat(i,1) *mat(i,2)^2 *(1 - mat(i,2)^3) + 4 *mat(i,2) *(2.25 - mat(i,1)* (1 - mat(i,2)^2)) + 6 *mat(i,2)^2 *(2.625 - mat(i,1) *(1 - mat(i,2)^3)), -2* mat(i,1) + 8* mat(i,1)^2 *mat(i,2)^2 + 18 *mat(i,1)^2 *mat(i,2)^4 + 4* mat(i,1) *(2.25 - mat(i,1) *(1 - mat(i,2)^2)) + 12 *mat(i,1) *mat(i,2)* (2.625 - mat(i,1) *(1 - mat(i,2)^3))];
     J=[2 *(mat(i,2)^3 - 1)* (2.625 - mat(i,1)* (1 - mat(i,2)^3)) + 2* (mat(i,2)^2 - 1)* (2.25 - mat(i,1)* (1 - mat(i,2)^2)) - (1 - mat(i,2))^2 ; 4* mat(i,1)* mat(i,2)* (2.25 - mat(i,1)* (1 - mat(i,2)^2)) + 6* mat(i,1)* mat(i,2)^2* (2.625 - mat(i,1)* (1 - mat(i,2)^3)) + 2* mat(i,1)* (1 - mat(i,2))];
-    p=H\J;
-    mat(i+1,:) = mat(i,:)-alpha(i)*p';
     EfPO=(1.5-mat(i+1,1)*(1-mat(i+1,2))^2)+(2.25-mat(i+1,1)*(1-mat(i+1,2)^2))^2+(2.625-mat(i+1,1)*(1-mat(i+1,2)^3))^2;
     Ef  =(1.5-mat(i,1)*(1-mat(i,2))^2)+(2.25-mat(i,1)*(1-mat(i,2)^2))^2+(2.625-mat(i,1)*(1-mat(i,2)^3))^2;
+    p=H\J;
+    deltaTR=norm(p);
+    P=-inv(H+mu*eye(2))*J;
+    rho=(Ef-EfPO)/(emZero-em(i));
+    if ((0<rho)&&(rho<0.25))
+        deltaTR=0.25*deltaTR;
+    elseif (rho>0.75)
+        deltaTR=2*deltaTR;
+    elseif ((0.25<rho)&&(rho<0.75))
+        
+    end
+    if (rho>eta)
+    mat(i+1,:) = mat(i,:)-alpha(i)*p';
+    else
+        mat(i+1,:) = mat(i,:);
+    end
     if (EfPO > Ef+ delta*alpha*sum(J'*p))
           alpha(i+1)=epsilon*alpha(i); 
     else
