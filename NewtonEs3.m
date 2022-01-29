c073d75a78aa50c483e4ef7764d094d26895e851
macrandrea <58860052+macrandrea@users.noreply.github.com>
2022-01-29 19:07:48 +0100

    Add files via upload

--- NewtonEs3.m
+++ NewtonEs3.m
@@ -1,22 +1,23 @@
 clc,clear
-maxit=1000;
+maxit=50;
 tol=1e-8;
 mat=zeros(maxit+1,2);
 alpha=ones(maxit,1);
 x0=[8,0.8];
 mat(1,:)=x0;
 delta=0.25;
-epsilon=0.7;
+epsilon=0.75;
 for i=1:maxit
-    H=[2*(mat(i,2)^2 - 1)^2 + 2*(mat(i,2)^3 - 1)^2, 4*mat(i,2)*(mat(i,1)*(mat(i,2)^2 - 1) + 9/4) - 2*mat(i,2) + 6*mat(i,2)^2*(mat(i,1)*(mat(i,2)^3 - 1) + 21/8) + 4*mat(i,1)*mat(i,2)*(mat(i,2)^2 - 1) + 6*mat(i,1)*mat(i,2)^2*(mat(i,2)^3 - 1) + 2; 4*mat(i,2)*(mat(i,1)*(mat(i,2)^2 - 1) + 9/4) - 2*mat(i,2) + 6*mat(i,2)^2*(mat(i,1)*(mat(i,2)^3 - 1) + 21/8) + 4*mat(i,1)*mat(i,2)*(mat(i,2)^2 - 1) + 6*mat(i,1)*mat(i,2)^2*(mat(i,2)^3 - 1) + 2 ,8*mat(i,1)^2*mat(i,2)^2 - 2*mat(i,1) + 18*mat(i,1)^2*mat(i,2)^4 + 4*mat(i,1)*(mat(i,1)*(mat(i,2)^2 - 1) + 9/4) + 12*mat(i,1)*mat(i,2)*(mat(i,1)*(mat(i,2)^3 - 1) + 21/8)];
-    J=[2*(mat(i,2)^2 - 1)*(mat(i,1)*(mat(i,2)^2 - 1) + 9/4) + 2*(mat(i,2)^3 - 1)*(mat(i,1)*(mat(i,2)^3 - 1) + 21/8) - (mat(i,2) - 1)^2; 6*mat(i,1)*mat(i,2)^2*(mat(i,1)*(mat(i,2)^3 - 1) + 21/8) - mat(i,1)*(2*mat(i,2) - 2) + 4*mat(i,1)*mat(i,2)*(mat(i,1)*(mat(i,2)^2 - 1) + 9/4)];
+    H=[2*(1 - mat(i,2)^2)^2 + 2*(1 - mat(i,2)^3)^2, 2* (1 - mat(i,2)) - 4 *mat(i,1)* mat(i,2)* (1 - mat(i,2)^2) - 6* mat(i,1)* mat(i,2)^2* (1 - mat(i,2)^3) + 4 *mat(i,2) *(2.25 - mat(i,1)* (1 - mat(i,2)^2)) + 6 *mat(i,2)^2* (2.625 - mat(i,1) *(1 - mat(i,2)^3)); 2 *(1 - mat(i,2)) - 4* mat(i,1)* mat(i,2) *(1 - mat(i,2)^2) - 6 *mat(i,1) *mat(i,2)^2 *(1 - mat(i,2)^3) + 4 *mat(i,2) *(2.25 - mat(i,1)* (1 - mat(i,2)^2)) + 6 *mat(i,2)^2 *(2.625 - mat(i,1) *(1 - mat(i,2)^3)), -2* mat(i,1) + 8* mat(i,1)^2 *mat(i,2)^2 + 18 *mat(i,1)^2 *mat(i,2)^4 + 4* mat(i,1) *(2.25 - mat(i,1) *(1 - mat(i,2)^2)) + 12 *mat(i,1) *mat(i,2)* (2.625 - mat(i,1) *(1 - mat(i,2)^3))];
+    J=[2 *(mat(i,2)^3 - 1)* (2.625 - mat(i,1)* (1 - mat(i,2)^3)) + 2* (mat(i,2)^2 - 1)* (2.25 - mat(i,1)* (1 - mat(i,2)^2)) - (1 - mat(i,2))^2 ; 4* mat(i,1)* mat(i,2)* (2.25 - mat(i,1)* (1 - mat(i,2)^2)) + 6* mat(i,1)* mat(i,2)^2* (2.625 - mat(i,1)* (1 - mat(i,2)^3)) + 2* mat(i,1)* (1 - mat(i,2))];
     p=H\J;
     mat(i+1,:) = mat(i,:)-alpha(i)*p';
     EfPO=(1.5-mat(i+1,1)*(1-mat(i+1,2))^2)+(2.25-mat(i+1,1)*(1-mat(i+1,2)^2))^2+(2.625-mat(i+1,1)*(1-mat(i+1,2)^3))^2;
     Ef  =(1.5-mat(i,1)*(1-mat(i,2))^2)+(2.25-mat(i,1)*(1-mat(i,2)^2))^2+(2.625-mat(i,1)*(1-mat(i,2)^3))^2;
-    diff=EfPO -Ef;
-    if (diff > delta*alpha(i)*sum(J'*p))
-          alpha(i+1)=epsilon*alpha(i);%alpha(i)=alpha(i);
+    if (EfPO > Ef+ delta*alpha*sum(J'*p))
+          alpha(i+1)=epsilon*alpha(i); 
+    else
+          alpha(i+1)=alpha(i);
         %if (alpha(i)<0.19)
         %   
         %end
